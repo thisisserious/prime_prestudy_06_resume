6316a915116e82600a35150f6301dd1d019f9e64
Laura Abend <laura.l.abend@gmail.com>
2016-09-18 18:29:31 -0500

    add jQuery & buttons

---
+++ scripts.js
@@ -0,0 +1,24 @@
+$('.button, .skillsButton, .expButton, .eduButton').mouseenter(function() {
+  $(this).fadeTo('fast', 1);
+});
+
+$('.button, .skillsButton, .expButton, .eduButton').mouseleave(function() {
+  $(this).fadeTo('slow', 0.65);
+});
+
+$('.button').click(function() {
+  $('.resume').slideToggle('fast');
+  $(this).hide('fast');
+});
+
+$('.skillsButton').click(function() {
+  $('#skillsList').slideToggle('slow');
+});
+
+$('.expButton').click(function() {
+  $('.experience').slideToggle('slow');
+});
+
+$('.eduButton').click(function() {
+  $('.education').slideToggle('slow');
+});
